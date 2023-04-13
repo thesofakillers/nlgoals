74ee3813058aa01d5d477c35bbda79b3c3ff9d9e
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-04-13 12:46:34 +0200

    support specifying path

--- data/calvin/download_data.sh
+++ data/calvin/download_data.sh
@@ -1,39 +1,45 @@
 #!/bin/bash
 
-# Download, Unzip, and Remove zip
-if [ "$1" = "D" ]
+if [ "$1" = "-h" ]
 then
+    echo "Usage: download_data.sh [-h] [D | ABC | ABCD | debug] [path]"
+    echo "Downloads and unzips datasets for tasks D, ABC, ABCD or debug."
+    echo "Optional argument [path] specifies the path where to download and unzip the file."
+    exit 0
+fi
 
-    echo "Downloading task_D_D ..."
-    wget http://calvin.cs.uni-freiburg.de/dataset/task_D_D.zip
-    unzip task_D_D.zip
-    echo "saved folder: task_D_D"
-elif [ "$1" = "ABC" ]
+if [ $# -lt 1 ]
 then
-
-    echo "Downloading task_ABC_D ..."
-    wget http://calvin.cs.uni-freiburg.de/dataset/task_ABC_D.zip
-    unzip task_ABC_D.zip
-    echo "saved folder: task_ABC_D"
-
-elif [ "$1" = "ABCD" ]
-then
-
-    echo "Downloading task_ABCD_D ..."
-    wget http://calvin.cs.uni-freiburg.de/dataset/task_ABCD_D.zip
-    unzip task_ABCD_D.zip
-    echo "saved folder: task_ABCD_D"
-
-elif [ "$1" = "debug" ]
-then
-
-    echo "Downloading debug dataset ..."
-    wget http://calvin.cs.uni-freiburg.de/dataset/calvin_debug_dataset.zip
-    unzip calvin_debug_dataset.zip
-    echo "saved folder: calvin_debug_dataset"
-
-
-else
-    echo "Failed: Usage download_data.sh D | ABC | ABCD | debug"
+    echo "Error: Missing argument. Usage download_data.sh [-h] [D | ABC | ABCD | debug] [path]"
     exit 1
 fi
+
+if [ "$1" = "D" ]
+then
+    echo "Downloading task_D_D ..."
+    wget http://calvin.cs.uni-freiburg.de/dataset/task_D_D.zip -P "$2"
+    unzip -q "$2/task_D_D.zip" -d "$2"
+    echo "Saved folder: $2/task_D_D"
+elif [ "$1" = "ABC" ]
+then
+    echo "Downloading task_ABC_D ..."
+    wget http://calvin.cs.uni-freiburg.de/dataset/task_ABC_D.zip -P "$2"
+    unzip -q "$2/task_ABC_D.zip" -d "$2"
+    echo "Saved folder: $2/task_ABC_D"
+elif [ "$1" = "ABCD" ]
+then
+    echo "Downloading task_ABCD_D ..."
+    wget http://calvin.cs.uni-freiburg.de/dataset/task_ABCD_D.zip -P "$2"
+    unzip -q "$2/task_ABCD_D.zip" -d "$2"
+    echo "Saved folder: $2/task_ABCD_D"
+elif [ "$1" = "debug" ]
+then
+    echo "Downloading debug dataset ..."
+    wget http://calvin.cs.uni-freiburg.de/dataset/calvin_debug_dataset.zip -P "$2"
+    unzip -q "$2/calvin_debug_dataset.zip" -d "$2"
+    echo "Saved folder: $2/calvin_debug_dataset"
+else
+    echo "Error: Invalid argument. Usage download_data.sh [-h] [D | ABC | ABCD | debug] [path]"
+    exit 1
+fi
+
