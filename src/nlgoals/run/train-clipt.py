624ee85199726901cf606e4ac3fde38307e7c9f6
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-04-26 16:59:18 +0200

    update train-clipt and trainer config

--- src/nlgoals/run/train-clipt.py
+++ src/nlgoals/run/train-clipt.py
@@ -1,9 +1,12 @@
 """Training of CLIPh: Contrastive Language Image Pretraining for Trajectories"""
 import jsonargparse
+import os
+
+import pytorch_lightning as pl
 
 from nlgoals.models.clipt import CLIPT
 from nlgoals.trainer import TrainerConfig
-from nlgoals.data.calvin import CALVIN
+from nlgoals.data.calvin.datamodule import CALVINDM
 
 
 def train(args):
@@ -12,21 +15,45 @@ def train(args):
     Instantiates model
     Trains model using contrastive loss between image traj and text pairs
     """
-    calvin_dm = CALVIN(**args.data.as_dict())
+    pl.seed_everything(args.seed)
+
+    calvin_dm = CALVINDM(**args.data.as_dict())
     model = CLIPT(**args.clipts.as_dict())
 
-    for epoch in range(args.trainer.n_epochs):
-        print(f"Epoch {epoch + 1} of {args.trainer.n_epochs}")
-        # TODO
-        pass
+    script_host = "slurm" if "SLURM_JOB_ID" in os.environ else "local"
+
+    logger = pl.loggers.WandbLogger(
+        job_type="train",
+        entity="giulio-uva",
+        project="nlgoals",
+        mode="disabled" if args.trainer.logging.disable else "online",
+        group=script_host,
+        config=args,
+        log_model=False,
+    )
+    early_stopping = pl.callbacks.early_stopping.EarlyStopping(
+        monitor="val_loss", mode="min"
+    )
+
+    trainer = pl.Trainer(
+        callbacks=[early_stopping],
+        logger=logger,
+        max_epochs=args.trainer.max_epochs,
+        devices=args.trainer.devices,
+        accelerator=args.trainer.accelerator,
+    )
+    trainer.fit(model, calvin_dm)
 
 
 if __name__ == "__main__":
     parser = jsonargparse.ArgumentParser(description=__doc__)
 
     parser.add_class_arguments(CLIPT, "clipt")
-    parser.add_class_arguments(CALVIN, "data")
+    parser.add_class_arguments(CALVINDM, "data")
     parser.add_argument("--trainer", type=TrainerConfig, default=TrainerConfig())
+    parser.add_argument("--seed", type=int, default=42)
+    parser.link_arguments("seed", "data.seed", apply_on="parse")
+
     args = parser.parse_args()
-    print(args)
-    # train(args)
+
+    train(args)
--- src/nlgoals/trainer.py
+++ src/nlgoals/trainer.py
@@ -1,9 +1,18 @@
 """
 Trainer config and class
 """
+from typing import Union
 from dataclasses import dataclass
 
 
 @dataclass
+class LoggingConfig:
+    disable: bool = False
+
+
+@dataclass
 class TrainerConfig:
-    n_epochs: int = 100
+    max_epochs: int = 50
+    accelerator: Union["cpu", "gpu", "tpu", "ipu", "auto"] = "auto"
+    devices: int = 1
+    logging: LoggingConfig = LoggingConfig()
