283aefe9a15d4ca3f94f7243a0dabcbf9421163c
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-04-26 10:44:26 +0200

    minor variable renames and docs for readability

--- src/nlgoals/data/utils.py
+++ src/nlgoals/data/utils.py
@@ -5,7 +5,7 @@ import transformers
 
 class CLIPTransform:
     """
-    Transforms for CLIP
+    Transforms for CLIP: tokenizing text, resizing, rescaling images
     """
 
     def __init__(self, clip_model: str, image_cols: List[str], text_col: str):
@@ -13,26 +13,29 @@ class CLIPTransform:
         self.text_col = text_col
         self.clip_processor = transformers.CLIPProcessor.from_pretrained(clip_model)
 
-    def __call__(self, sample: Dict):
+    def __call__(self, unproc_input: Dict):
         """
         Works with both single and batched samples
         """
-        processed_sample = {}
-        for col in sample.keys():
+        proc_output = {}
+        for col in unproc_input.keys():
             if col in self.image_cols:
-                processed_sample[col] = self.clip_processor(
-                    images=sample[col],
+                proc_output[col] = self.clip_processor(
+                    images=unproc_input[col],
                     return_tensors="pt",
                 )
             elif col == self.text_col:
-                if type(sample[col]) == str:
-                    input_text = [sample[col]]
+                if type(unproc_input[col]) == str:
+                    input_text = [unproc_input[col]]
                 else:
-                    input_text = sample[col]
-                processed_sample[col] = self.clip_processor(
+                    input_text = unproc_input[col]
+                proc_output[col] = self.clip_processor(
                     text=input_text, return_tensors="pt", padding=True
                 )
             else:
-                processed_sample[col] = sample[col]
+                proc_output[col] = unproc_input[col]
 
-        return processed_sample
+        return proc_output
+
+
+TRANSFORM_MAP: Dict[str, object] = {"clip-transform": CLIPTransform}
