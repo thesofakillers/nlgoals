08e9d954a41c34d767a5b56c6c1214ee0793febf
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-05-19 16:00:37 +0200

    add tags to distinguish between train runs

--- src/nlgoals/run/train-clipt.py
+++ src/nlgoals/run/train-clipt.py
@@ -44,6 +44,7 @@ def train(args):
         group=script_host,
         config=args,
         log_model=False,
+        tags=["clipt"],
     )
     early_stopping = pl.callbacks.early_stopping.EarlyStopping(
         monitor="val_loss", mode="min"
--- src/nlgoals/run/train-task_classifier.py
+++ src/nlgoals/run/train-task_classifier.py
@@ -64,6 +64,7 @@ def train(args):
         group=script_host,
         config=args,
         log_model=False,
+        tags=['task_classifier']
     )
     early_stopping = pl.callbacks.early_stopping.EarlyStopping(
         monitor="textual/val_accuracy", mode="max"
