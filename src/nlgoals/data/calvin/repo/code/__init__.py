6e72054adae1895bd003819b1d06dbe69be1dc6d
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-06-06 11:56:07 +0200

    transfer calvin_agent.utils too, cuz why not

---
+++ src/nlgoals/data/calvin/repo/code/__init__.py
@@ -0,0 +1,7 @@
+"""
+This directory contains copied and then slightly adjusted code from the following:
+
+https://github.com/mees/calvin
+'CALVIN - A benchmark for Language-Conditioned Policy Learning for Long-Horizon Robot Manipulation Tasks
+:copyright: 2021 by Oier Mees
+"""
