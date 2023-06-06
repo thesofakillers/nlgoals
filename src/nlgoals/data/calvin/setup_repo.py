f0081089d01c0ee08d91e2be65666a2d8b0cf0ad
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-06-06 15:59:46 +0200

    file for seting up repo

---
+++ src/nlgoals/data/calvin/setup_repo.py
@@ -0,0 +1,18 @@
+"""Script for setting up repo dataset"""
+import hydra
+import os
+from omegaconf import DictConfig, OmegaConf
+
+
+@hydra.main(version_base=None, config_path="./repo/conf/", config_name="datamodule")
+def main(cfg: DictConfig):
+    datamodule = hydra.utils.instantiate(
+        cfg, training_repo_root="/Users/thesofakillers/repos/thesis/"
+    )
+
+    datamodule.prepare_data()
+    datamodule.setup()
+
+
+if __name__ == "__main__":
+    main()
