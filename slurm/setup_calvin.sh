6776637e2970c11a78e9c2ee04e78c735533498a
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-06-15 09:53:38 +0200

    propagate fact that we parse source as arg

--- slurm/setup_calvin.sh
+++ slurm/setup_calvin.sh
@@ -2,23 +2,26 @@
 
 # Copies calvin .zip data from /scratch-shared/ to /scratch-node/
 # Unzips .zip data into the data folder
-# Takes ~ 20 minutes to run
+# Takes ~ 20 minutes to run for task_D_D (~166GB)
 
-# define source file (passed as arg) and target dir
-source_file=$1
+# parse source file (passed as arg)
+source_path=$1
+source_file_name=$(basename $source_path)
+source_file_name_no_ext="${source_file_name%.*}"
+# define target dir
 target_dir=$TMPDIR/data
 
 # make the target dir
 mkdir -p $target_dir
 
 echo "Copying to $target_dir"
-cp $source_file $target_dir
+cp $source_path $target_dir
 echo "Done."
 
 # unzip
 echo "Unzipping..."
-unzip -q $target_dir/task_D_D.zip -d $target_dir
+unzip -q $target_dir/$source_file_name -d $target_dir
 echo "Done."
 
-$data_dir=$target_dir/task_D_D/
+$data_dir=$target_dir/$source_file_name_no_ext/
 echo "Data files ready in $data_dir"
