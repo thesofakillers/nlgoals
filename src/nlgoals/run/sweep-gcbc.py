2f04cd98567944947342cce7d73d8e4d9d8cca1f
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-06-21 21:48:32 +0200

    get rid of redundant init

--- src/nlgoals/run/sweep-gcbc.py
+++ src/nlgoals/run/sweep-gcbc.py
@@ -65,64 +65,63 @@ def train(args):
         log_model=False,
         tags=["gcbc"],
     )
-    with wandb.init() as run:
-        # override config with wandb sweep config
-        args.gcbc.lr = run.config["lr"]
-        args.gcbc.hidden_dim = run.config["hidden_dim"]
+    print(logger.experiment.config)
 
-        if args.trainer.accelerator == "auto":
-            device = "cuda" if torch.cuda.is_available() else "cpu"
-        else:
-            device = args.trainer.accelerator
-        # determinism
-        pl.seed_everything(args.seed, workers=True)
-        # datamodule
-        hydra.core.global_hydra.GlobalHydra.instance().clear()
-        hydra.initialize_config_module(
-            config_module="nlgoals.data.calvin.repo.conf.datamodule"
-        )
-        datamodule_cfg = hydra.compose(
-            config_name=args.data.config_name,
-            overrides=[]
-            if args.data.shared_memory is True
-            else ["datasets=vision_lang"],
-        )
-        datamodule_cfg.batch_size = args.data.batch_size
-        datamodule_cfg.num_workers = args.data.num_workers
-        datamodule_cfg.root_data_dir = args.data.data_dir
-        datamodule = hydra.utils.instantiate(datamodule_cfg)
-        datamodule.collator.custom_collate_fn = calvin_gcbc_collate
-        # model
-        model = GCBC(
-            traj_encoder_kwargs=args.clipt.as_dict(),
-            vision_encoder_kwargs=args.vision_encoder.as_dict(),
-            proprio_encoder_kwargs=args.proprio_encoder.as_dict(),
-            **args.gcbc.as_dict(),
-        )
-        if args.clipt_checkpoint is not None:
-            clipt_state_dict = torch.load(args.clipt_checkpoint, map_location=device)[
-                "state_dict"
-            ]
-            clipt = CLIPT(**args.clipt.as_dict())
-            clipt.load_state_dict(clipt_state_dict, strict=False)
-            model.set_traj_encoder(clipt)
-        model.prepare_visual_batch = calvin_gcbc_visual
-        model.prepare_textual_batch = calvin_gcbc_textual
-        # trainer
-        trainer = pl.Trainer(
-            max_epochs=2,
-            accelerator=args.trainer.accelerator,
-            devices=args.trainer.devices,
-            enable_progress_bar=args.trainer.enable_progress_bar,
-            deterministic=True,
-            logger=logger,
-            log_every_n_steps=args.trainer.log_every_n_steps,
-            val_check_interval=args.trainer.val_check_interval,
-            check_val_every_n_epoch=args.trainer.check_val_every_n_epoch,
-            precision=args.trainer.precision,
-        )
+    # override config with wandb sweep config
+    args.gcbc.lr = logger.experiment.config["lr"]
+    args.gcbc.hidden_dim = logger.experiment.config["hidden_dim"]
 
-        trainer.fit(model, datamodule)
+    if args.trainer.accelerator == "auto":
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+    else:
+        device = args.trainer.accelerator
+    # determinism
+    pl.seed_everything(args.seed, workers=True)
+    # datamodule
+    hydra.core.global_hydra.GlobalHydra.instance().clear()
+    hydra.initialize_config_module(
+        config_module="nlgoals.data.calvin.repo.conf.datamodule"
+    )
+    datamodule_cfg = hydra.compose(
+        config_name=args.data.config_name,
+        overrides=[] if args.data.shared_memory is True else ["datasets=vision_lang"],
+    )
+    datamodule_cfg.batch_size = args.data.batch_size
+    datamodule_cfg.num_workers = args.data.num_workers
+    datamodule_cfg.root_data_dir = args.data.data_dir
+    datamodule = hydra.utils.instantiate(datamodule_cfg)
+    datamodule.collator.custom_collate_fn = calvin_gcbc_collate
+    # model
+    model = GCBC(
+        traj_encoder_kwargs=args.clipt.as_dict(),
+        vision_encoder_kwargs=args.vision_encoder.as_dict(),
+        proprio_encoder_kwargs=args.proprio_encoder.as_dict(),
+        **args.gcbc.as_dict(),
+    )
+    if args.clipt_checkpoint is not None:
+        clipt_state_dict = torch.load(args.clipt_checkpoint, map_location=device)[
+            "state_dict"
+        ]
+        clipt = CLIPT(**args.clipt.as_dict())
+        clipt.load_state_dict(clipt_state_dict, strict=False)
+        model.set_traj_encoder(clipt)
+    model.prepare_visual_batch = calvin_gcbc_visual
+    model.prepare_textual_batch = calvin_gcbc_textual
+    # trainer
+    trainer = pl.Trainer(
+        max_epochs=2,
+        accelerator=args.trainer.accelerator,
+        devices=args.trainer.devices,
+        enable_progress_bar=args.trainer.enable_progress_bar,
+        deterministic=True,
+        logger=logger,
+        log_every_n_steps=args.trainer.log_every_n_steps,
+        val_check_interval=args.trainer.val_check_interval,
+        check_val_every_n_epoch=args.trainer.check_val_every_n_epoch,
+        precision=args.trainer.precision,
+    )
+
+    trainer.fit(model, datamodule)
 
 
 if __name__ == "__main__":
