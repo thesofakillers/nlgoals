3d4352d71e1efe79ec08281dae01e8f77ce97ebe
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-07-13 18:35:48 +0200

    update clipt test

--- tests/models/clipt_test.py
+++ tests/models/clipt_test.py
@@ -10,7 +10,7 @@ from nlgoals.models.clipt import CLIPT
 @pytest.fixture
 def clipt():
     batch_size = 2
-    num_frames = 4
+    num_frames = 2
     text_len = 10
     emb_dim = 512
 
@@ -29,7 +29,12 @@ def clipt():
 def test_forward(clipt):
     model, _, _, images, text_input_ids, text_attn_mask, _ = clipt
 
-    output = model(images, text_input_ids, text_attn_mask)
+    batch = {
+        "images": images,
+        "text_input_ids": text_input_ids,
+        "text_attn_mask": text_attn_mask,
+    }
+    output = model(batch)
 
     visual_traj_emb, text_traj_emb = output["visual_traj_emb"], output["text_traj_emb"]
 
@@ -41,9 +46,10 @@ def test_initialization(clipt):
     model, _, _, _, _, _, _ = clipt
 
     assert isinstance(model.clip_model, transformers.CLIPModel)
-    assert isinstance(model.traj_encoder, nn.Sequential)
+    assert isinstance(model.visual_traj_encoder, nn.Sequential)
+    assert isinstance(model.textual_traj_encoder, nn.Sequential)
     assert model.emb_dim == model.clip_model.config.projection_dim
-    assert model.num_frames == 4
+    assert model.num_frames == 2
 
 
 def test_training(clipt):
@@ -51,7 +57,12 @@ def test_training(clipt):
 
     optimizer.zero_grad()
 
-    output = model(images, text_input_ids, text_attn_mask)
+    batch = {
+        "images": images,
+        "text_input_ids": text_input_ids,
+        "text_attn_mask": text_attn_mask,
+    }
+    output = model(batch)
     loss = criterion(output["visual_traj_emb"], targets)
     loss.backward()
     optimizer.step()
