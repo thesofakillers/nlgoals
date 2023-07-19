c4e6073f1f5bd5eea531378ee2ae4d10c3533600
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-07-19 17:23:27 +0200

    simplify and modernize demos script [wip]

--- src/nlgoals/babyai/scripts/make_agent_demos.py
+++ src/nlgoals/babyai/scripts/make_agent_demos.py
@@ -11,16 +11,15 @@ make_agent_demos.py at your cluster as --job-script and the number of jobs as --
 """
 
 import argparse
-import gym
 import logging
-import sys
-import subprocess
-import os
 import time
+
+import gymnasium as gym
+from minigrid.envs.babyai.core.roomgrid_level import RoomGridLevel
+from minigrid.utils.baby_ai_bot import BabyAIBot
 import numpy as np
 import blosc
 import torch
-from torch._C import _import_ir_module_from_package
 
 import nlgoals.babyai.utils as utils
 
@@ -34,21 +33,21 @@ def print_demo_lengths(demos):
     )
 
 
-def generate_demos(n_episodes, valid, seed, shift=0):
+def generate_demos(n_episodes, valid, seed):
     utils.seed(seed)
 
-    # Generate environment
-    env = gym.make(args.env)
+    # env instance
+    env: RoomGridLevel = gym.make(args.env)
 
-    agent = utils.load_agent(
-        env, args.model, args.demos, "agent", args.argmax, args.env
-    )
     demos_path = utils.get_demos_path(args.demos, args.env, "agent", valid)
     demos = []
 
     checkpoint_time = time.time()
 
+    curr_seed = seed
+
     just_crashed = False
+    # start generating episodes (demos)
     while True:
         if len(demos) == n_episodes:
             break
@@ -58,11 +57,11 @@ def generate_demos(n_episodes, valid, seed, shift=0):
             logger.info(
                 "reset the environment to find a mission that the bot can solve"
             )
-            env.reset()
+            curr_seed += 1
         else:
-            env.seed(seed + len(demos))
-        obs = env.reset()
-        agent.on_reset()
+            curr_seed = curr_seed
+        obs = env.reset(seed=curr_seed)[0]
+        agent = BabyAIBot(env)
 
         actions = []
         mission = obs["mission"]
@@ -71,17 +70,17 @@ def generate_demos(n_episodes, valid, seed, shift=0):
 
         try:
             while not done:
-                action = agent.act(obs)["action"]
+                action = agent.replan()
                 if isinstance(action, torch.Tensor):
                     action = action.item()
-                new_obs, reward, done, _ = env.step(action)
-                agent.analyze_feedback(reward, done)
+                new_obs, reward, done, _, _ = env.step(action)
 
                 actions.append(action)
                 images.append(obs["image"])
                 directions.append(obs["direction"])
 
                 obs = new_obs
+            # if our demos was succesful, save it
             if reward > 0 and (
                 args.filter_steps == 0 or len(images) <= args.filter_steps
             ):
@@ -89,12 +88,10 @@ def generate_demos(n_episodes, valid, seed, shift=0):
                     (mission, blosc.pack_array(np.array(images)), directions, actions)
                 )
                 just_crashed = False
-
+            # handle unsuccessful demos
             if reward == 0:
                 if args.on_exception == "crash":
-                    raise Exception(
-                        "mission failed, the seed is {}".format(seed + len(demos))
-                    )
+                    raise Exception("mission failed, the seed is {}".format(curr_seed))
                 just_crashed = True
                 logger.info("mission failed")
         except (Exception, AssertionError):
@@ -104,6 +101,7 @@ def generate_demos(n_episodes, valid, seed, shift=0):
             logger.exception("error while generating demo #{}".format(len(demos)))
             continue
 
+        # logging progress
         if len(demos) and len(demos) % args.log_interval == 0:
             now = time.time()
             demos_per_second = args.log_interval / (now - checkpoint_time)
@@ -115,8 +113,7 @@ def generate_demos(n_episodes, valid, seed, shift=0):
             )
             checkpoint_time = now
 
-        # Save demonstrations
-
+        # Save demos
         if (
             args.save_interval > 0
             and len(demos) < n_episodes
@@ -125,75 +122,13 @@ def generate_demos(n_episodes, valid, seed, shift=0):
             logger.info("Saving demos...")
             utils.save_demos(demos, demos_path)
             logger.info("{} demos saved".format(len(demos)))
-            # print statistics for the last 100 demonstrations
             print_demo_lengths(demos[-100:])
 
-    # Save demonstrations
+    # Save last batch of demos
     logger.info("Saving demos...")
     utils.save_demos(demos, demos_path)
     logger.info("{} demos saved".format(len(demos)))
     print_demo_lengths(demos[-100:])
-
-
-def generate_demos_cluster():
-    demos_per_job = args.episodes // args.jobs
-    demos_path = utils.get_demos_path(args.demos, args.env, "agent")
-    job_demo_names = [
-        os.path.realpath(demos_path + ".shard{}".format(i)) for i in range(args.jobs)
-    ]
-    for demo_name in job_demo_names:
-        job_demos_path = utils.get_demos_path(demo_name)
-        if os.path.exists(job_demos_path):
-            os.remove(job_demos_path)
-
-    command = [args.job_script]
-    command += sys.argv[1:]
-    for i in range(args.jobs):
-        cmd_i = list(
-            map(
-                str,
-                command
-                + ["--seed", args.seed + i * demos_per_job]
-                + ["--demos", job_demo_names[i]]
-                + ["--episodes", demos_per_job]
-                + ["--jobs", 0]
-                + ["--valid-episodes", 0],
-            )
-        )
-        logger.info("LAUNCH COMMAND")
-        logger.info(cmd_i)
-        output = subprocess.check_output(cmd_i)
-        logger.info("LAUNCH OUTPUT")
-        logger.info(output.decode("utf-8"))
-
-    job_demos = [None] * args.jobs
-    while True:
-        jobs_done = 0
-        for i in range(args.jobs):
-            if job_demos[i] is None or len(job_demos[i]) < demos_per_job:
-                try:
-                    logger.info("Trying to load shard {}".format(i))
-                    job_demos[i] = utils.load_demos(
-                        utils.get_demos_path(job_demo_names[i])
-                    )
-                    logger.info(
-                        "{} demos ready in shard {}".format(len(job_demos[i]), i)
-                    )
-                except Exception:
-                    logger.exception("Failed to load the shard")
-            if job_demos[i] and len(job_demos[i]) == demos_per_job:
-                jobs_done += 1
-        logger.info("{} out of {} shards done".format(jobs_done, args.jobs))
-        if jobs_done == args.jobs:
-            break
-        logger.info("sleep for 60 seconds")
-        time.sleep(60)
-
-    # Training demos
-    all_demos = []
-    for demos in job_demos:
-        all_demos.extend(demos)
-    utils.save_demos(all_demos, demos_path)
 
 
 if __name__ == "__main__":
@@ -257,26 +192,13 @@ if __name__ == "__main__":
         help="How to handle exceptions during demo generation",
     )
 
-    parser.add_argument(
-        "--job-script",
-        type=str,
-        default=None,
-        help="The script that launches make_agent_demos.py at a cluster.",
-    )
-    parser.add_argument(
-        "--jobs", type=int, default=0, help="Split generation in that many jobs"
-    )
-
     args = parser.parse_args()
     logger = logging.getLogger(__name__)
 
     logging.basicConfig(level="INFO", format="%(asctime)s: %(levelname)s: %(message)s")
     logger.info(args)
     # Training demos
-    if args.jobs == 0:
-        generate_demos(args.episodes, False, args.seed)
-    else:
-        generate_demos_cluster()
+    generate_demos(args.episodes, False, args.seed)
     # Validation demos
     if args.valid_episodes:
         generate_demos(args.valid_episodes, True, int(1e9))
