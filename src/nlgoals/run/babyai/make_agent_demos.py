addc5f3e8c862d29b940f139a42828d25d297c3d
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-07-26 13:12:09 +0200

    generate demos from a mix of environments; other
    other:
    
    - save reward and env name too
    - minor documentation fixes

--- src/nlgoals/run/babyai/make_agent_demos.py
+++ src/nlgoals/run/babyai/make_agent_demos.py
@@ -1,13 +1,8 @@
-#!/usr/bin/env python3
-
 """
-Generate a set of agent demonstrations.
+Generate a set of agent demonstrations from the BabyAIBot
 
-The agent can either be a trained model or the heuristic expert (bot).
-
-Demonstration generation can take a long time, but it can be parallelized
-if you have a cluster at your disposal. Provide a script that launches
-make_agent_demos.py at your cluster as --job-script and the number of jobs as --jobs.
+Modification of the original make_agent_demos.py
+https://github.com/mila-iqia/babyai/blob/master/scripts/make_agent_demos.py
 """
 
 import argparse
@@ -24,9 +19,27 @@ import torch
 
 import nlgoals.babyai.utils as utils
 
+SIZE_TO_ENVS = {
+    "large": [
+        "BabyAI-GoToOpen-v0",
+        "BabyAI-Unlock-v0",
+        "BabyAI-Pickup-v0",
+        "BabyAI-Open-v0",
+        "BabyAI-SynthLoc-v0",
+        "BabyAI-Synth-v0",
+    ],
+    "small": [
+        "BabyAI-GoToObj-v0",
+        "BabyAI-GoToLocal-v0",
+        "BabyAI-PickupDist-v0",
+        "BabyAI-PickupLoc-v0",
+        "BabyAI-PutNextLocal-v0",
+    ],
+}
+
 
 def print_demo_lengths(demos):
-    num_frames_per_episode = [len(demo[2]) for demo in demos]
+    num_frames_per_episode = [len(demo[3]) for demo in demos]
     logger.info(
         "Demo length: {:.3f}+-{:.3f}".format(
             np.mean(num_frames_per_episode), np.std(num_frames_per_episode)
@@ -34,14 +47,21 @@ def print_demo_lengths(demos):
     )
 
 
-def generate_demos(n_episodes, valid, seed):
+def generate_demos(n_episodes: int, valid: bool, seed: int, envs_size: str):
+    """
+    Generate a set of agent demonstrations from the BabyAIBot
+
+    Args:
+        n_episodes (int): number of episodes to generate
+        valid (bool): whether to the episodes are for validation or not
+        seed (int): random starting seed
+        envs_size (str): Which environment size to use. Can be "small" or "large"
+    """
     utils.seed(seed)
 
-    # env instance
-    env: RoomGridLevel = gym.make(args.env, highlight=False)
-    env = RGBImgObsWrapper(env)
+    possible_envs = SIZE_TO_ENVS[envs_size]
 
-    demos_path = utils.get_demos_path(args.save_path, args.env, "agent", valid)
+    demos_path = utils.get_demos_path(args.save_path, None, "agent", valid)
     print(demos_path)
     demos = []
 
@@ -54,6 +74,11 @@ def generate_demos(n_episodes, valid, seed):
     while True:
         if len(demos) == n_episodes:
             break
+
+        # sample a random environment
+        env_name = np.random.choice(possible_envs)
+        env: RoomGridLevel = gym.make(env_name, highlight=False)
+        env = RGBImgObsWrapper(env, tile_size=28 if envs_size == "small" else 12)
 
         done = False
         final_step = False
@@ -99,6 +124,7 @@ def generate_demos(n_episodes, valid, seed):
                 demos.append(
                     (
                         mission,
+                        env_name,
                         blosc.pack_array(np.array(images)),
                         directions,
                         actions,
@@ -157,10 +183,10 @@ if __name__ == "__main__":
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
     parser.add_argument(
-        "--env", required=True, help="name of the environment to be run (REQUIRED)"
-    )
-    parser.add_argument(
-        "--model", default="BOT", help="name of the trained model (REQUIRED)"
+        "--envs-size",
+        choices=["small", "large"],
+        default="small",
+        help="Whether to use small or large environments",
     )
     parser.add_argument(
         "--save-path",
@@ -202,7 +228,10 @@ if __name__ == "__main__":
         "--filter-steps",
         type=int,
         default=0,
-        help="filter out demos with number of steps more than filter-steps",
+        help=(
+            "filter out demos with number of steps more than filter-steps. "
+            "Ignored if 0."
+        ),
     )
     parser.add_argument(
         "--on-exception",
@@ -218,7 +247,7 @@ if __name__ == "__main__":
     logging.basicConfig(level="INFO", format="%(asctime)s: %(levelname)s: %(message)s")
     logger.info(args)
     # Training demos
-    generate_demos(args.episodes, False, args.seed)
+    generate_demos(args.episodes, False, args.seed, args.envs_size)
     # Validation demos
     if args.valid_episodes:
-        generate_demos(args.valid_episodes, True, int(1e9))
+        generate_demos(args.valid_episodes, True, int(1e9), args.envs_size)
