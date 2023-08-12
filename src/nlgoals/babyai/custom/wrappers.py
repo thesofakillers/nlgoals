ab32b0c4748f3b657d1dd77b610ac8494fc93a22
Giulio Starace <26286291+thesofakillers@users.noreply.github.com>
2023-08-12 14:47:03 +0200

    implement DistractorConstraintWrapper

--- src/nlgoals/babyai/custom/wrappers.py
+++ src/nlgoals/babyai/custom/wrappers.py
@@ -34,27 +34,118 @@ class ColorObjLockWrapper(Wrapper):
 
         return obs
 
+
 class DistractorConstraintWrapper(Wrapper):
     """
-    # TODO
+    Ensures that there are at either
+    - at least `min_type` distractors of type `obj_type` in the environment
+    - at least `min_color` distractors of color `color` in the environment
     """
 
     def __init__(
         self,
         env,
-        min_obj: Optional[int] = None,
-        obj_type: Optional[str] = None,
-        track_objects: Optional[bool] = False,
-        min_color: Optional[int] = None,
-        color: Optional[str] = None,
-        track_colors: Optional[bool] = False,
+        min_type=None,
+        obj_type=None,
+        track_types=False,
+        min_color=None,
+        color=None,
+        track_colors=False,
     ):
+        """
+        Args:
+            env: The environment to wrap.
+            min_type: min num of distractors of type `obj_type` that must be in the env.
+            obj_type: The object type to count.
+            track_types: track the positions of objects of type `obj_type` in the env.
+                in self.tracked_type_positions.
+            min_color: min num of distractors of color `color` that must be in the env.
+            color: The color to count.
+            track_colors: track the positions of objects of color `color` in the env.
+                in self.tracked_color_positions.
+
+        """
         super().__init__(env)
-        self.min_obj = min_obj
+        self.min_type = min_type
         self.obj_type = obj_type
-        self.track_objects = track_objects
+        self.track_types = track_types
         self.min_color = min_color
         self.color = color
         self.track_colors = track_colors
         self.wrapper_name = "distractor-constraint"
-        raise NotImplementedError
+
+    def reset(self, **kwargs):
+        obs = self.env.reset(**kwargs)
+
+        self.tracked_type_positions = [] if self.track_types else None
+        self.tracked_color_positions = [] if self.track_colors else None
+
+        type_count, color_count = self._count_and_track()
+        self._ensure_minimums(type_count, color_count)
+
+        return obs
+
+    def _count_and_track(self):
+        type_count = color_count = 0
+        for distractor in self.unwrapped.distractors:
+            type_count, color_count = self._update_counts_and_positions(
+                distractor, type_count, color_count
+            )
+        return type_count, color_count
+
+    def _update_counts_and_positions(self, distractor, type_count, color_count):
+        if self.obj_type and distractor.type == self.obj_type:
+            type_count += 1
+            if self.tracked_type_positions is not None:
+                self.tracked_type_positions.append(distractor.cur_pos)
+        if self.color and distractor.color == self.color:
+            color_count += 1
+            if self.tracked_color_positions is not None:
+                self.tracked_color_positions.append(distractor.cur_pos)
+        return type_count, color_count
+
+    def _ensure_minimums(self, type_count, color_count):
+        if self.min_type:
+            self._ensure_minimum(
+                "type",
+                self.min_type,
+                self.obj_type,
+                type_count,
+                self.tracked_type_positions,
+            )
+        if self.min_color:
+            self._ensure_minimum(
+                "color",
+                self.min_color,
+                self.color,
+                color_count,
+                self.tracked_color_positions,
+            )
+
+    def _ensure_minimum(self, attr, min_value, attr_value, count, positions):
+        """
+        If the number of distractors of `attr`, `attr_value` is less than `min_value`,
+        tries to replace other distractors with distractors of `attr`, `attr_value`.
+
+        Args:
+            attr: "type" or "color"
+            min_value: The minimum number of distractors of `attr`, `attr_value`
+            attr_value: The value of `attr` to count.
+            count: The number of distractors of `attr`, `attr_value` in the environment.
+            positions: The positions of distractors of `attr`, `attr_value` in the environment.
+                either self.tracked_type_positions or self.tracked_color_positions.
+        """
+        if count < min_value:
+            if len(self.unwrapped.distractors) < (min_value - count):
+                raise ValueError(
+                    f"Cannot ensure minimum {min_value} of {attr} {attr_value}. Not enough distractors."
+                )
+            else:
+                for distractor in self.unwrapped.distractors:
+                    if getattr(distractor, attr) != attr_value:
+                        setattr(distractor, attr, attr_value)
+                        count += 1
+                        if positions is not None:
+                            positions.append(distractor.cur_pos)
+                    if count >= min_value:
+                        break
